488cb055e9a85ee4bb5a44682587e3563d93a184
kylestech95 <kylestech95@outlook.com>
2024-03-13 07:35:25 -0400

    feat: passport authenticate

--- server.js
+++ server.js
@@ -3,13 +3,18 @@ require('dotenv').config();
 const passport = require('passport')
 const LocalStrategy = require('passport-local').Strategy
 const routes = require('./lib/routes.js')
-
 // express, session, cors & body-parser
 const express = require('express')
 const session = require('express-session')
 const cors = require('cors')
 const bodyParser = require('body-parser')
 const users = []
+const initializePassport = require('./lib/passport-config')
+const flash = require('express-flash')
+
+initializePassport(passport,email => users.find(user=>user.email===email))
+
+
 // define app
 const app = express();
 app.set('view-engine','ejs')
@@ -27,6 +32,8 @@ app.use(express.json())
 app.use(express.urlencoded({extended:true}))
 app.use(bodyParser.urlencoded({extended:true}))
 app.use('/public', express.static(`${process.cwd()}/public`));
-
+app.use(session({
+    secret:process.env.SESSION_KEY
+}))
 
 routes(app,users)
